1f6470c839e1316435b6a4ad33cb7282ba215800
Ghograshan <aktass.ardaa@gmail.com>
2023-04-21 01:05:54 +0300

    player movement sprites added, created player, entity, gamepanel and keyhandler classes

---
+++ src/main/GamePanel.java
@@ -0,0 +1,82 @@
+package main;
+
+import entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    //SCREEN SETTING
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    //FPS
+    int FPS = 60;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = (double) 1000000000 /FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        player.draw(g2);
+
+        g2.dispose();
+    }
+}
---
+++ src/main/KeyHandler.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        else if (code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        else if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        else if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
+}
