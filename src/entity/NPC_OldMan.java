9ae326f800d6bce0ccd8bcfd3a2ad4c99f19aa68
Ghograshan <aktass.ardaa@gmail.com>
2023-05-28 19:48:06 +0300

    #17

---
+++ src/entity/NPC_OldMan.java
@@ -0,0 +1,70 @@
+package entity;
+
+import main.GamePanel;
+import main.UtilityTool;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Random;
+
+public class NPC_OldMan extends Entity{
+
+    public NPC_OldMan(GamePanel gp){
+        super(gp);
+
+        direction = "down";
+        speed = 1;
+
+        getImage();
+        setDialogue();
+    }
+
+    public void getImage() {
+        up1 = setup("/npc/oldman_up_1");
+        up2 = setup("/npc/oldman_up_2");
+        down1 = setup("/npc/oldman_down_1");
+        down2 = setup("/npc/oldman_down_2");
+        left1 = setup("/npc/oldman_left_1");
+        left2 = setup("/npc/oldman_left_2");
+        right1 = setup("/npc/oldman_right_1");
+        right2 = setup("/npc/oldman_right_2");
+    }
+
+    public void setDialogue() {
+        dialogues[0] = "Hello, lad.";
+        dialogues[1] = "So you've come to this island to \nfind the treasure?";
+        dialogues[2] = "I used to be a great wizard but \nnow... I'm a bit too old for taking \nan adventure.";
+        dialogues[3] = "Well, good luck on you.";
+
+    }
+
+    public void setAction() {
+        actionLockCounter++;
+
+        if (actionLockCounter == 120){
+            Random random = new Random();
+            int i = random.nextInt(100)+1;
+
+            if (i <= 25) {
+                direction = "up";
+            }
+            if (i > 25 && i <= 50){
+                direction = "down";
+            }
+            if (i > 50 && i <= 75){
+                direction = "left";
+            }
+            if (i > 75 && i <= 100){
+                direction = "right";
+            }
+            actionLockCounter = 0;
+        }
+    }
+
+    public void speak() {
+        super.speak();
+    }
+
+}
